27801c1ed178ced5e9de96ffb3488dbd7e33ff3d
Catalin Andrei Dobre <cadobredev@gmail.com>
2018-06-17 17:14:40 +0300

    #42 rename web-frame folder to web-frame-app

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './web-frame/web-frame-app.module';
+import { AppModule } from './web-frame-app/web-frame-app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
