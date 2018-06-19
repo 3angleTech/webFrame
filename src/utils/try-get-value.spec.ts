b07afd55d9a9ecde62f620e0e6e239ca77cf90b1
Catalin Andrei Dobre <cadobredev@gmail.com>
2018-06-19 14:11:56 +0300

    #11 implemented tryGetValue

---
+++ src/utils/try-get-value.spec.ts
@@ -0,0 +1,44 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestObject } from 'protractor/built/driverProviders';
+import { tryGetValue } from 'src/utils/try-get-value';
+
+
+describe('tryGetValue', () => {
+  let testObject;
+
+  beforeAll(() => {
+    testObject = {
+      childA: {
+        childA1: 'A1',
+        childA2: 'A2',
+        childA3: 3
+      },
+      childB: {
+        childB1: 'B1',
+        childB2: {
+          childB21Null: null,
+          childB22Undefined: undefined,
+          childB23: {
+            childB231: 'B231'
+          }
+        }
+      }
+    };
+  });
+
+  it('should return null if no value is found for the provided member access chain', () => {
+    expect(tryGetValue(testObject, 'childX')).toBeNull();
+    expect(tryGetValue(testObject, 'childA.X')).toBeNull();
+    expect(tryGetValue(testObject, 'childB.childB2.childB21Null')).toBeNull();
+    expect(tryGetValue(testObject, 'childB.childB2.childB22Undefined')).toBeNull();
+    expect(tryGetValue(testObject, 'childB.childB2.X')).toBeNull();
+  });
+
+  it('should retrieve and return the value for the provided member access chain', () => {
+    expect(tryGetValue(testObject, 'childA')).toEqual(testObject.childA);
+    expect(tryGetValue(testObject, 'childA.childA1')).toEqual(testObject.childA.childA1);
+    expect(tryGetValue(testObject, 'childB')).toEqual(testObject.childB);
+    expect(tryGetValue(testObject, 'childB.childB2')).toEqual(testObject.childB.childB2);
+    expect(tryGetValue(testObject, 'childB.childB2.childB23.childB231')).toEqual(testObject.childB.childB2.childB23.childB231);
+  });
+});
--- src/utils/try-get-value.ts
+++ src/utils/try-get-value.ts
@@ -1,2 +1,23 @@
-export function tryGetValue() {
+import { isNil } from '.';
+
+/**
+ * Traverse the object as defined in the memberAccessChain and try to get it's value is it's defined. If the member is not define
+ * it will fail safe by returning a null value.
+ * @param object The target object
+ * @param memberAccessChain The chain of access members of the object separated by the member access operator "."
+ * e.g. memberAccessChain = "a.b" will return the value of object[a][b].
+ */
+export function tryGetValue<T>(object: Object, memberAccessChain: string): T {
+  if (isNil(object, memberAccessChain)) {
+    return null;
+  }
+
+  let currentValue = object;
+  const keyChain = memberAccessChain.split('.');
+
+  for (const key of keyChain) {
+    currentValue = currentValue[key];
+  }
+
+  return <T>currentValue;
 }
