615cd974d268c33ef3e96daaa3480087576d89f0
Schneider Werner Walter <contact@schnwalter.eu>
2018-07-23 14:23:42 +0300

    #67 Move `shared` features and code into the `app-shared` directory.

---
+++ src/app-shared/core/index.ts
@@ -0,0 +1,6 @@
+/**
+ * This file contains an explicit list of things exported in the current directory.
+ */
+
+export { SerializableDataModel } from './data/serializable-data-model';
+export { User } from './data/user.dm';
