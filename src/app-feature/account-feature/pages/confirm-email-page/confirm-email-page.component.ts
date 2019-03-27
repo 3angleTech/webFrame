6a59fa9d0c88886ae8ab71ce638654d7f869732b
Razvan Lapadus <razvan@ezops.com>
2019-03-27 15:16:26 +0200

    #54 [Web] Implement email confirmed page

--- src/app-feature/account-feature/pages/confirm-email-page/confirm-email-page.component.ts
+++ src/app-feature/account-feature/pages/confirm-email-page/confirm-email-page.component.ts
@@ -9,16 +9,25 @@
  */
 import { Component, OnInit } from '@angular/core';
 
+export enum TokenVerificationStatusType {
+  InProgress,
+  ValidToken,
+  TokenExpired,
+  InvalidToken,
+}
+
 @Component({
   selector: 'app-confirm-email-page',
   templateUrl: './confirm-email-page.component.html',
   styleUrls: ['./confirm-email-page.component.scss'],
 })
 export class ConfirmEmailPageComponent implements OnInit {
+  public tokenStatus: TokenVerificationStatusType = TokenVerificationStatusType.ValidToken;
+  // tslint:disable-next-line:typedef
+  public tokenVerificationStatusType = TokenVerificationStatusType;
 
   constructor() { }
 
   public ngOnInit(): void {
   }
-
 }
