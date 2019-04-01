f0cef94781ccbb03d643124d03f30ee63bf80b3e
Ionut Cristian Paraschiv <ionut@3angle.tech>
2019-04-01 17:44:21 +0300

    #98 Fixed production api base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -11,7 +11,7 @@
 export const environment = {
   production: true,
   clientId: '3at-api',
-  apiBaseUrl: 'http://localhost:3000/api/v1',
+  apiBaseUrl: '/api/v1',
   appName: 'WebFrame-3angleTECH',
   clientSecret: '5r5rd15c650f4849119eb894939d9fdaaf5f7d0e7e0f65de15b71bfa6411011y'
 };
