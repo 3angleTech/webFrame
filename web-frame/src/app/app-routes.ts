8586b2352ec1a74c3b698a1005998830f2a2bae4
Walter Werner SCHNEIDER <contact@schnwalter.eu>
2020-05-21 04:40:01 +0300

    #122 Move source files to web-frame.

--- web-frame/src/app/app-routes.ts
+++ web-frame/src/app/app-routes.ts
@@ -19,15 +19,15 @@ export const APP_ROUTES: Routes = [
   },
   {
     path: 'account',
-    loadChildren: () => import('src/app-feature/account-feature/account-feature.module').then(m => m.AccountFeatureModule),
+    loadChildren: () => import('app-feature/account-feature/account-feature.module').then(m => m.AccountFeatureModule),
   },
   {
     path: 'profile',
     canActivate: [AuthenticatedGuard],
-    loadChildren: () => import('src/app-feature/profile-feature/profile-feature.module').then(m => m.ProfileFeatureModule),
+    loadChildren: () => import('app-feature/profile-feature/profile-feature.module').then(m => m.ProfileFeatureModule),
   },
   {
     path: 'sandbox',
-    loadChildren: () => import('src/app-feature/sandbox-feature/sandbox-feature.module').then(m => m.SandboxFeatureModule),
+    loadChildren: () => import('app-feature/sandbox-feature/sandbox-feature.module').then(m => m.SandboxFeatureModule),
   },
 ];
