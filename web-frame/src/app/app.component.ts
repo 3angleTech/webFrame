a86522b3a9b55b2f256c599715b4ab735c556052
Walter Werner SCHNEIDER <contact@schnwalter.eu>
2020-05-21 20:25:11 +0300

    #125 Cleanup the AppComponent.

--- web-frame/src/app/app.component.ts
+++ web-frame/src/app/app.component.ts
@@ -11,8 +11,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  template: '<router-outlet></router-outlet>',
 })
 export class AppComponent {
 }
