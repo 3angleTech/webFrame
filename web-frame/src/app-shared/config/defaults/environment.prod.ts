7d6522bb582f2df5e16cafed81c36f7d02bc5e7b
Walter Werner SCHNEIDER <contact@schnwalter.eu>
2020-05-25 17:51:35 +0300

    #126 Update comments.

--- web-frame/src/app-shared/config/defaults/environment.prod.ts
+++ web-frame/src/app-shared/config/defaults/environment.prod.ts
@@ -1,11 +1,12 @@
 /**
+ * @file This file should never be imported directly, see the fileReplacements.
+ *
  * @license
  * Copyright (c) 2020 THREEANGLE SOFTWARE SOLUTIONS SRL
  * Available under MIT license webFrame/LICENSE
  */
 import { IEnvironmentConfig } from '../interfaces/environment-config.interface';
 
-/** @internal This variable should never be used directly; only by the build process. */
 export const ENVIRONMENT: Readonly<IEnvironmentConfig> = {
   apiBaseUrl: '/api/v1',
   appBaseUrl: '/',
