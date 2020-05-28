aa908f9f739111203d3e5bade8e378f78fbc51fb
Walter Werner SCHNEIDER <contact@schnwalter.eu>
2020-05-29 00:54:12 +0300

    #121 Update comments.

--- web-frame/src/app-shared/config/interfaces/environment-config.interface.ts
+++ web-frame/src/app-shared/config/interfaces/environment-config.interface.ts
@@ -9,6 +9,9 @@ export interface IEnvironmentConfig {
    * The Base URL where the application is served. It can be an absolute URL, or a relative URL from the domain root.
    *
    * The path for the base URL needs to match the base element href attribute defined in index.html.
+   *
+   * NOTE: The URL must end with a forward-slash (/).
+   * TODO: Improve DX when using appBaseUrl. It should always be required when building URLs.
    */
   appBaseUrl: string;
   appName: string;
